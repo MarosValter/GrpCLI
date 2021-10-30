2c1d42f5fe6716a8cf7f8421fa8c29ce44c455c2
Maros Valter <maros.valter@thermofisher.com>
2021-10-30 02:51:51 +0200

    Cleanup

--- GrpCLI/Program.cs
+++ GrpCLI/Program.cs
@@ -1,9 +1,4 @@
-﻿using Google.Protobuf.Reflection;
-using System;
-using System.CommandLine;
-using System.CommandLine.Invocation;
-using System.Linq;
-using System.Reflection;
+﻿using System.CommandLine;
 using System.Threading.Tasks;
 
 namespace GrpCLI
@@ -14,33 +9,6 @@ namespace GrpCLI
         {
             var cliRoot = new GrpCLI.CliRoot("https://localhost:5001");
             await cliRoot.InvokeAsync(args);
-
-            //var descriptor = GetProtoDescriptor();
-            //var service = descriptor.Services[0];
-            //var method = service.Methods[2];
-            //var input = method.InputType;
-            //var field = input.Fields[1];
-            ////HmProvider.HmProviderClient
-            //var protoPath = Environment.GetEnvironmentVariable("PROTO_PATH");
-
-            //Console.WriteLine(JsonSerializer.Serialize(service.FullName));
-            //Console.WriteLine(JsonSerializer.Serialize(method.FullName));
-            //Console.WriteLine(JsonSerializer.Serialize(input.FullName));
-            //Console.WriteLine(JsonSerializer.Serialize(field.FullName));
-            //Console.ReadKey();
         }
-
-        //private static FileDescriptor GetProtoDescriptor()
-        //{
-        //    var descriptors = Assembly.GetExecutingAssembly()
-        //        .ExportedTypes
-        //        .Select(t =>
-        //            t.GetProperty("Descriptor", BindingFlags.Public | BindingFlags.Static, null, typeof(FileDescriptor), Array.Empty<Type>(), null))
-        //        .Where(x => x != null)
-        //        .Select(x => (FileDescriptor)x.GetValue(null))
-        //        .ToList();
-
-        //    return descriptors.FirstOrDefault();
-        //}
     }
 }
